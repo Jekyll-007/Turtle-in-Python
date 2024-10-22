9f0e41f146f2c0e85be2a183349721ade1ef60fd
Jekyll-007 <teenofeejay@gmail.com>
2024-10-22 18:09:37 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+#creating a canvas
+turtle.Screen().bgcolor("blue")
+sc=turtle.Screen()
+sc.setup(500,500)
+turtle.title("Welcome to Turtle Window!!")
+board=turtle.Turtle()
+for i in range(4):
+    board.forward(100)
+    board.left(90)
+    i=1+1
+turtle.done()
